744957c903a55de5433b1c5e877dd8c8d2e594e3
yhaae01 <suryha7fold@gmail.com>
2020-05-14 04:28:32 +0700

    Memperbaiki tampilan navbar

--- application/views/layouts/navbar.php
+++ application/views/layouts/navbar.php
@@ -10,17 +10,17 @@
                 <li class="nav-item active">
                     <a class="nav-link" href="<?= base_url('home'); ?>">Beranda <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        Manage
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown1">
-                        <a href="<?= base_url('category'); ?>" class="dropdown-item">Kategori</a>
-                        <a href="<?= base_url('product'); ?>" class="dropdown-item">Produk</a>
-                        <a href="<?= base_url(''); ?>" class="dropdown-item">Order</a>
-                        <a href="<?= base_url('user'); ?>" class="dropdown-item">Pengguna</a>
-                    </div>
+                <li class="nav-item ">
+                    <a class="nav-link" href="<?= base_url('category'); ?>" class="dropdown-item">Kategori</a>
+                </li>
+                <li class="nav-item "> 
+                    <a class="nav-link" href="<?= base_url('product'); ?>" class="dropdown-item">Produk</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="<?= base_url(''); ?>" class="dropdown-item">Order</a>                   
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="<?= base_url('user'); ?>" class="dropdown-item">Pengguna</a>  
                 </li>
             </ul>
             <ul class="navbar-nav">
@@ -44,7 +44,7 @@
                     <div class="dropdown-menu" aria-labelledby="dropdown2">
                         <a href="<?= base_url('Profile'); ?>" class="dropdown-item">Profile</a>
                         <a href="<?= base_url('myorder'); ?>" class="dropdown-item">Pesanan</a>
-                        <a href="<?= base_url('/logout'); ?>" class="dropdown-item">keluar</a>
+                        <a href="<?= base_url('/logout'); ?>" class="dropdown-item">Keluar</a>
                     </div>
                 </li>
                 <?php endif; ?>
