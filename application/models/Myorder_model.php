4b861c3612412a92028cb212311857d7de1b8345
yhaae01 <suryha7fold@gmail.com>
2020-05-14 09:18:55 +0700

    Membuat validasi rules di myorder_model

--- application/models/Myorder_model.php
+++ application/models/Myorder_model.php
@@ -6,6 +6,68 @@ class Myorder_model extends MY_Model
 
     public $table = 'orders';
 
+    public function getDefaultValues()
+    {
+        return [
+            'id_order'          => '',
+            'account_name'      => '',
+            'account_number'    => '',
+            'nominal'           => '',
+            'note'              => '',
+            'image'             => ''
+        ];
+    }
+
+    public function getValidationRules()
+    {
+        $validationRules = [
+            [
+                'field' => 'account_name',
+                'label' => 'Nama Pemilik',
+                'rules' => 'trim|required'
+            ],
+            [
+                'field' => 'account_number',
+                'label' => 'Nomor Rekening',
+                'rules' => 'trim|required|numeric|max_length[50]'
+            ],
+            [
+                'field' => 'nominal',
+                'label' => 'Nominal',
+                'rules' => 'trim|required|numeric'
+            ],
+            [
+                'field' => 'image',
+                'label' => 'Bukti Transfer',
+                'rules' => 'callback_image_required'
+            ]
+        ];
+
+        return $validationRules;
+    }
+
+    public function uploadImage($fieldName, $fileName)
+    {
+        $config['upload_path']      = './assets/images/confirm';
+        $config['file_name']        = $fileName;
+        $config['allowed_types']    = 'gif|jpg|png|JPG|PNG|jpeg';
+        $config['max_size']         = 1024;
+        $config['max_width']        = 0;
+        $config['max_height']       = 0;
+        $config['overwrite']        = true;
+        $config['file_ext_tolower'] = true;
+        
+        $this->load->library('upload', $config);
+        
+        if ($this->upload->do_upload($fieldName)){
+            return $this->upload->data();
+        }
+        else{
+            $this->session->set_flashdata('image_error', $this->upload->display_errors('', ''));
+            return false;
+        }
+    }
+
 }
 
 /* End of file Myorder_model.php */
