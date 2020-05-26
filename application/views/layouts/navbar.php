785e9efe479b9b86075af5f54bdaaf5cd36d83e5
yhaae01 <suryha7fold@gmail.com>
2020-05-26 18:35:49 +0700

    Mengubah posisi keranjang, akan muncul saat login

--- application/views/layouts/navbar.php
+++ application/views/layouts/navbar.php
@@ -27,9 +27,6 @@
                 <?php endif; ?>
             </ul>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="<?= base_url('cart'); ?>" class="nav-link"> <i class="fas fa-shopping-cart"></i> Keranjang (<?= getCart(); ?>) </a>
-                </li>
 
                 <?php if (!$this->session->userdata('is_login')) : ?>
                 <li class="nav-item">
@@ -39,6 +36,9 @@
                     <a href="<?= base_url('/register'); ?>" class="nav-link">Register</a>
                 </li>
                 <?php else : ?>
+                <li class="nav-item">
+                    <a href="<?= base_url('cart'); ?>" class="nav-link"> <i class="fas fa-shopping-cart"></i> Keranjang (<?= getCart(); ?>) </a>
+                </li>
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" id="dropdown2" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
