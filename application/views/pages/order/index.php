87630ab7b233142f064778fea69b046bd5856c48
yhaae01 <suryha7fold@gmail.com>
2020-06-10 18:58:40 +0700

    Menambahkan tombol detail di order/index

--- application/views/pages/order/index.php
+++ application/views/pages/order/index.php
@@ -36,7 +36,6 @@
                             </tr>
                         </thead>
                         <tbody>
-<<<<<<< HEAD
                         <?php foreach ($content as $row) :?>
                             <tr>
                                 <td>
@@ -52,20 +51,6 @@
                                 </td>
                             </tr>
                         <?php endforeach; ?>
-=======
-                            <?php foreach ($content as $row) : ?>
-                                <tr>
-                                    <td>
-                                        <a href="<?= base_url("order/detail/$row->id"); ?>"><strong>#<?= $row->invoice; ?></strong></a>
-                                    </td>
-                                    <td> <?= str_replace('-', '/', date("d-m-Y", strtotime($row->date))); ?> </td>
-                                    <td> Rp.<?= number_format($row->total, 0, ',', '.') ?>,- </td>
-                                    <td>
-                                        <?php $this->load->view('layouts/status', ['status' => $row->status]); ?>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
->>>>>>> d8c8d6ec109f7678517a5afc3c718b5d3923f613
                         </tbody>
                     </table>
 
