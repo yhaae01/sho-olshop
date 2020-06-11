b1a21597778d076667d450d37b2cab7a0e5c458c
yhaae01 <suryha7fold@gmail.com>
2020-06-11 19:39:03 +0700

    Menambahkan icon di home admin

--- application/views/pages/admin/index.php
+++ application/views/pages/admin/index.php
@@ -12,7 +12,7 @@
                     <h5 class="card-text mb-3">Belum Dibayar 
                         <p class="badge badge-pill badge-primary">
                             <?= 
-                            $this->Admin_model->getOrderWhere(['status' => 'waiting'])->num_rows(); 
+                                $this->Admin_model->getOrderWhere(['status' => 'waiting'])->num_rows(); 
                             ?>
                         </p> 
                     </h5>
@@ -43,23 +43,7 @@
                             $this->Admin_model->getOrderWhere(['total' >= 1])->num_rows(); 
                         ?> -->
                     <!-- </h3> -->
-                    <a href="<?= base_url('order'); ?>" class="btn btn-outline-primary btn-block btn-sm">Detail</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4">
-            <div class="card">
-                <div class="card-header">
-                    <h5>Total Pengguna</h5>
-                </div>
-                <div class="card-body">
-                    <h3 class="card-text text-center mb-3">
-                        <?= 
-                            $this->Admin_model->getUserWhere(['role' => 'member'])->num_rows(); 
-                        ?>
-                    </h3>
-                    <a href="<?= base_url('user'); ?>" class="btn btn-outline-primary btn-block btn-sm">Detail</a>
+                    <a href="<?= base_url('order'); ?>" class="btn btn-outline-info btn-block btn-sm">Detail</a>
                 </div>
             </div>
         </div>
@@ -71,11 +55,29 @@
                 </div>
                 <div class="card-body">
                     <h3 class="card-text text-center mb-3">
+                    <i class="fas fa-tags mr-3"></i>
                         <?= 
                             $this->Admin_model->getProductWhere(['is_available' => 1])->num_rows(); 
                         ?>
                     </h3>
-                    <a href="<?= base_url('product'); ?>" class="btn btn-outline-primary btn-block btn-sm">Detail</a>
+                    <a href="<?= base_url('product'); ?>" class="btn btn-outline-info btn-block btn-sm">Detail</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Total Pengguna</h5>
+                </div>
+                <div class="card-body">
+                    <h3 class="card-text text-center mb-3">
+                    <i class="fas fa-users mr-3"></i>
+                        <?= 
+                            $this->Admin_model->getUserWhere(['role' => 'member'])->num_rows(); 
+                        ?>
+                    </h3>
+                    <a href="<?= base_url('user'); ?>" class="btn btn-outline-info btn-block btn-sm">Detail</a>
                 </div>
             </div>
         </div>
