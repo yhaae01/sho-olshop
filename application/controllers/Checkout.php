afe40dcb8fcfc489d7594dc61313bcb02541e93c
ahmadmaulana10 <ahmadmaulana.am96@gmail.com>
2020-06-14 00:20:09 +0700

    concate address

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -1,36 +1,36 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Checkout extends MY_Controller 
+class Checkout extends MY_Controller
 {
-    private $id;
+	private $id;
 
-    public function __construct()
-    {
-        parent::__construct();
-        $is_login   = $this->session->userdata('is_login');
-        $this->id   = $this->session->userdata('id');
+	public function __construct()
+	{
+		parent::__construct();
+		$is_login   = $this->session->userdata('is_login');
+		$this->id   = $this->session->userdata('id');
 
-        if (!$is_login) {
-            redirect(base_url());
-            return;
-        }
-    }
+		if (!$is_login) {
+			redirect(base_url());
+			return;
+		}
+	}
 
-    public function index($input = null)
-    {
+	public function index($input = null)
+	{
 		$this->checkout->table = 'cart';
 		$data['provinsi']	   = $this->checkout->getProv();
 
 		$data['cart']	= $this->checkout->select([
-				'cart.id', 'cart.qty', 'cart.subtotal',
-				'product.title', 'product.image', 'product.price'
-			])
+			'cart.id', 'cart.qty', 'cart.subtotal',
+			'product.title', 'product.image', 'product.price'
+		])
 			->join('product')
 			->where('cart.id_user', $this->id)
 			->get();
 
-		if (! $data['cart']) {
+		if (!$data['cart']) {
 			$this->session->set_flashdata('warning', 'Tidak ada produk di dalam keranjang.');
 			redirect(base_url('/'));
 		}
@@ -41,35 +41,36 @@ class Checkout extends MY_Controller
 
 		$this->view($data);
 	}
-	
+
 	public function getKab($id_prov)
 	{
-		$kab=$this->checkout->getKab($id_prov);
-		echo"<option value=''>Pilih Kota/Kab</option>";
-		foreach($kab as $k){
+		$kab = $this->checkout->getKab($id_prov);
+		echo "<option value=''>Pilih Kota/Kab</option>";
+		foreach ($kab as $k) {
 			echo "<option value='{$k->id_kab}'>{$k->nama}</option>";
 		}
 	}
-	
+
 	public function getKec($id_kab)
 	{
-		$kec=$this->checkout->getKec($id_kab);
-		echo"<option value=''>Pilih Kecamatan</option>";
-		foreach($kec as $k){
+		$kec = $this->checkout->getKec($id_kab);
+		echo "<option value=''>Pilih Kecamatan</option>";
+		foreach ($kec as $k) {
 			echo "<option value='{$k->id_kec}'>{$k->nama}</option>";
 		}
 	}
 
 	public function getKel($id_kec)
 	{
-		$kel=$this->checkout->getKel($id_kec);
-		echo"<option value=''>Pilih Kelurahan/Desa</option>";
-		foreach($kel as $k){
+		$kel = $this->checkout->getKel($id_kec);
+		echo "<option value=''>Pilih Kelurahan/Desa</option>";
+		foreach ($kel as $k) {
 			echo "<option value='{$k->id_kel}'>{$k->nama}</option>";
 		}
 	}
 
-    public function create()
+
+	public function create()
 	{
 		if (!$_POST) {
 			redirect(base_url('checkout'));
@@ -77,31 +78,37 @@ class Checkout extends MY_Controller
 			$input = (object) $this->input->post(null, true);
 		}
 
+		// get data provinsi, kabupaten, kelurahan, kecamatan by ID
+		$prop = $this->db->get_where('provinsi', ['id_prov' => $input->prop])->row()->nama;
+		$kab = $this->db->get_where('kabupaten', ['id_kab' => $input->kota])->row()->nama;
+		$kel = $this->db->get_where('kelurahan', ['id_kel' => $input->kel])->row()->nama;
+		$kec = $this->db->get_where('kecamatan', ['id_kec' => $input->kec])->row()->nama;
+
 		if (!$this->checkout->validate()) {
 			return $this->index($input);
 		}
 
 		$total	= $this->db->select_sum('subtotal')
-					->where('id_user', $this->id)
-					->get('cart')
-					->row()
-					->subtotal;
+			->where('id_user', $this->id)
+			->get('cart')
+			->row()
+			->subtotal;
 
 		$data = [
 			'id_user'	=> $this->id,
 			'date'		=> date('Y-m-d'),
-			'invoice'	=> $this->id.date('YmdHis'),
+			'invoice'	=> $this->id . date('YmdHis'),
 			'total'		=> $total,
 			'name'		=> $input->name,
-			'address'	=> $input->address,
+			'address'	=> $input->address . ', ' . $kec . ', ' . $kel . ', ' . $kab . ', ' . $prop,
 			'phone'		=> $input->phone,
 			'status'	=> 'waiting'
 		];
 
 		if ($order = $this->checkout->create($data)) {
 			$cart = $this->db->where('id_user', $this->id)
-					->get('cart')->result_array();
-			foreach($cart as $row) {
+				->get('cart')->result_array();
+			foreach ($cart as $row) {
 				$row['id_orders']	= $order;
 				unset($row['id'], $row['id_user']);
 				$this->db->insert('orders_detail', $row);
@@ -121,7 +128,6 @@ class Checkout extends MY_Controller
 			return $this->index($input);
 		}
 	}
-
 }
 
 /* End of file Checkout.php */
--- application/views/pages/checkout/index.php
+++ application/views/pages/checkout/index.php
@@ -2,24 +2,24 @@
     <?php $this->load->view('layouts/alert'); ?>
     <div class="row">
         <div class="col-md-8">
-        <h4 class="pt-3 pb-3">Keranjang Belanja</h4>
+            <h4 class="pt-3 pb-3">Keranjang Belanja</h4>
             <div class="card">
                 <div class="card-body">
                     <form action="<?= base_url("/checkout/create") ?>" method="POST">
-						<div class="form-group">
-							<label for="">Nama</label>
-							<input type="text" class="form-control" name="name" value="<?= $input->name ?>">
-							<?= form_error('name') ?>
-						</div>
-						<div class="form-row">
+                        <div class="form-group">
+                            <label for="">Nama</label>
+                            <input type="text" class="form-control" name="name" value="<?= $input->name ?>">
+                            <?= form_error('name') ?>
+                        </div>
+                        <div class="form-row">
                             <div class="form-group col-md-3">
                                 <label for="prop">Provinsi</label>
                                 <select id="prop" name="prop" class="form-control" onchange="ajaxkota(this.value)">
                                     <option selected>Pilih Provinsi</option>
-                                    <?php 
-                                        foreach($provinsi as $data){
-                                            echo '<option value="'.$data->id_prov.'">'.$data->nama.'</option>';
-                                        }
+                                    <?php
+                                    foreach ($provinsi as $data) {
+                                        echo '<option value="' . $data->id_prov . '">' . $data->nama . '</option>';
+                                    }
                                     ?>
                                 </select>
                             </div>
@@ -42,19 +42,19 @@
                                 </select>
                             </div>
                         </div>
-						<div class="form-group">
-							<label for="">Alamat Lengkap</label>
-							<textarea name="address" id="" cols="30" rows="5" class="form-control"><?= $input->address ?></textarea>
-							<?= form_error('address') ?>
-						</div>
-						<div class="form-group">
-							<label for="">Telepon</label>
-							<input type="text" class="form-control col-4" maxlength="13" onkeypress="return hanyaAngka(event)" name="phone" value="<?= $input->phone ?>">
-							<?= form_error('phone') ?>
-						</div>
+                        <div class="form-group">
+                            <label for="">Alamat Lengkap</label>
+                            <textarea name="address" id="" cols="30" rows="5" class="form-control"><?= $input->address ?></textarea>
+                            <?= form_error('address') ?>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Telepon</label>
+                            <input type="text" class="form-control col-4" maxlength="13" onkeypress="return hanyaAngka(event)" name="phone" value="<?= $input->phone ?>">
+                            <?= form_error('phone') ?>
+                        </div>
 
-						<button class="btn btn-primary" type="submit">Simpan</button>
-					</form>
+                        <button class="btn btn-primary" type="submit">Simpan</button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -77,17 +77,17 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                <?php foreach ($cart as $row) : ?>
-                                    <tr>
-                                        <td><?= $row->title; ?></td>
-                                        <td><?= $row->qty; ?></td>
-                                        <td>Rp.<?= number_format($row->price, 0, ',', '.'); ?>,-</td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2">Subtotal</td>
-                                        <td>Rp.<?= number_format($row->subtotal, 0, ',', '.'); ?>,-</td>
-                                    </tr>
-                                <?php endforeach; ?>
+                                    <?php foreach ($cart as $row) : ?>
+                                        <tr>
+                                            <td><?= $row->title; ?></td>
+                                            <td><?= $row->qty; ?></td>
+                                            <td>Rp.<?= number_format($row->price, 0, ',', '.'); ?>,-</td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2">Subtotal</td>
+                                            <td>Rp.<?= number_format($row->subtotal, 0, ',', '.'); ?>,-</td>
+                                        </tr>
+                                    <?php endforeach; ?>
                                 </tbody>
                                 <tfoot>
                                     <tr>
